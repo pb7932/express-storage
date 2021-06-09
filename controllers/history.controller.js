db42d90d05dad01f556e84d18f809b2dc5d3bf1a
pb7932 <pebe1928@gmail.com>
2021-06-09 18:30:38 +0200

    Creating a new history table and seeding it with received products

--- controllers/history.controller.js
+++ controllers/history.controller.js
@@ -3,13 +3,18 @@ const {query} = require('../db/index');
 const app = require('../server');
 
 exports.createHistory = async (req, res, next) => {
+    //get the new tables version, increase it and store it
     if( req.app.global.data.counter == undefined )
         req.app.global.data.counter = 0;
 
-    ++req.app.global.data.counter;
+    let counter = ++req.app.global.data.counter;
     req.app.global.store();
 
-    res.send('counter: ' + req.app.global.data.counter);
+    await createNewHistoryTable(counter);
+    
+    await seedHistoryTable(counter, req.body.products);
+
+    res.send('counter: ' + counter);
 }
 
 exports.initCounter = (req, res, next) => {
@@ -28,13 +33,44 @@ exports.getHistory = async (req, res, next) => {
         const sql = `SELECT * FROM history${version}`;
         const result = await (await query(sql, [])).rows;
 
-        console.log(result);
-
         res.send(result);
     }
     else {
         console.log('no history table has been made yet');
         res.send(null);
+    }
+}
+
+let createNewHistoryTable = async (version) => {
+    const sql = `CREATE TABLE history${version} (
+        id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        name TEXT NOT NULL UNIQUE,
+        price SMALLINT NOT NULL,
+        ingredients TEXT NOT NULL,
+        calories SMALLINT NOT NULL,
+        quantity INTEGER NOT NULL DEFAULT 1,
+        url TEXT NOT NULL 
+    )`
+
+    try {
+        await query(sql, []);
+    }
+    catch (err) {
+        console.log('An error occured while creating a history table: ' + err);
+    }
+} 
+
+let seedHistoryTable = async (version, products) => {
+    const sql = `INSERT INTO history${version} (name, price, ingredients, calories, quantity, url) 
+                                        values ($1, $2, $3, $4, $5, $6)`;
+
+    try {
+        for ( let product of products )
+            await query(sql,[product.name, product.price, product.ingredients, 
+                            product.calories, product.quantity, product.url]);
+    }
+    catch (err) {
+        console.log('An error occured while seeding the history table' + err);
     }
 }
     
